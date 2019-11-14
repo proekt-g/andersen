481f3d21ab520ba9f2b5f4594bbd12a01dad6827
Proekt_G <prog.dev.rob@gmail.com>
2019-11-14 12:18:50 +0200

    Сделайте функцию которая будет принимать массив других функций и будет соединять их в цепочку наследование (справа налево)

--- chainFamily.js
+++ chainFamily.js
@@ -1,46 +1,28 @@
 function chain(...args){
-    args.reverse();
     for(let i = 0; i < args.length - 1; i++){
-<<<<<<< HEAD
-        args[i].prototype = Object.create(args[i + 1].prototype);
+        args[i + 1].prototype.__proto__ = Object.create(args[i].prototype)
     }
-    return args[0].prototype;
+    return new args[args.length - 1]();
 }
 
 function Shape() {}
 function Polygon() {}
-Polygon.prototype.draw = function() {}
-=======
-        args[i].prototype = args[i + 1].prototype;
-    }
-    return args[0]
+function Square(sizeSize) {
+    this.sizeSize = sizeSize;
 }
-function Shape() {}
 Polygon.prototype.draw = function() {}
-function Polygon() {}
->>>>>>> ce69cee8a501ae0a299d70afcb059a97671bd3a4
 Polygon.prototype.getNumbersOfAngles = function() {
     return this.anglesNumber;
 }
 Polygon.prototype.setNumbersOfAngles = function(num) {
     this.anglesNumber = num;
 }
-function Square(sizeSize) {
-    this.sizeSize = sizeSize;
-}
 Square.prototype.getArea = function() {
     return this.sizeSize * this.sizeSize;
-}
+}   
 const ExtendedSquare = chain(Square, Polygon, Shape);
-<<<<<<< HEAD
 
 console.log(ExtendedSquare.getArea); // notToBe(undefined);
-// console.log(ExtendedSquare.setNumbersOfAngles); // notToBe(undefined);
-// console.log(ExtendedSquare.getNumbersOfAngles); // notToBe(undefined);
-// console.log(ExtendedSquare.draw); // notToBe(undefined);
-=======
-console.log(ExtendedSquare.getArea); // notToBe(undefined);
-// assert(ExtendedSquare.setNumbersOfAngles).notToBe(undefined);
-// assert(ExtendedSquare.getNumbersOfAngles).notToBe(undefined);    
-// assert(ExtendedSquare.draw).notToBe(undefined);
->>>>>>> ce69cee8a501ae0a299d70afcb059a97671bd3a4
+console.log(ExtendedSquare.setNumbersOfAngles); // notToBe(undefined);
+console.log(ExtendedSquare.getNumbersOfAngles); // notToBe(undefined);
+console.log(ExtendedSquare.draw); // notToBe(undefined);
