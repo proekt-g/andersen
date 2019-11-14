02d2bf8d7c522a7384a69c773d459135d2c3dd89
Proekt_G <prog.dev.rob@gmail.com>
2019-11-14 13:09:43 +0200

    Требуется создать функцию позволяющую подмешивать множество прототипов в итоговый объект

--- mixin.js
+++ mixin.js
@@ -1,21 +1,3 @@
-    function mixin(prototype1, prototype2){ 
-        prototype1 = Object.create(prototype2);
-        return Object.create(prototype1);
-    }
-
-function Shape() {}
-function Drawable() {}
-function Chainable() {} 
-const originalPrototype = Shape.prototype;
-Drawable.prototype.draw = function() {}
-Chainable.prototype.chain = function() {}
-Shape.prototype = mixin(Drawable.prototype, Chainable.prototype);
-// console.log(Shape.prototype); // (originalPrototype)
-// console.log(Shape.prototype.__proto__.__proto__); // (Object.prototype)
-// console.log(Drawable.prototype.__proto__); // (Object.prototype)
-// console.log(Shape.prototype.__proto__.__proto__); // (Object.prototype)
-// console.log(Chainable.prototype.__proto__); // (Object.prototype)
-// console.log(Shape.prototype.__proto__.draw); // (undefined)
-// console.log(Shape.prototype.__proto__.chain); // (undefined)
-// console.log(Shape.prototype.__proto__.chain); // (Chainable.prototype.chain)
-// console.log(Shape.prototype.__proto__.draw); // (Drawable.prototype.draw)
+function mixin(prototype1, prototype2){
+    return Object.create(Object.assign({}, prototype1, prototype2));
+}
