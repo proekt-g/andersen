3e0fb01e3f7d6b56c69ce0b1927e6a64be9d026d
Proekt_G <prog.dev.rob@gmail.com>
2019-12-02 20:34:06 +0200

    Сделать FormComponent класс. Этот класс должен иметь возможность исходя из принятых свойств построить форму и навесить на нее необходимые функции обработчики а так же менять эту форму при изменении свойств. Вам требуется создать модульИз которого будет возвращен класс компонента, имеющий функциональность для построения формы

---
+++ formComponent.js
@@ -0,0 +1,30 @@
+function module(){
+    class FormComponent {
+        constructor(element){
+            this.anchor = element;
+        }
+        onInit(){ }   
+        onPropsChanges() { }  
+        set props(value){
+            this._props = value;
+            if(this.anchor !== undefined) { 
+                this.anchor.appendChild(document.createElement("form"));
+                for(let i in this._props){
+                    let inputelement = document.createElement("input");
+                    inputelement.addEventListener("change", (event) => {
+                        this._props[i].onChanges(event.target.value);
+                    })
+                    inputelement.name = i;
+                    inputelement.value = this._props[i].value;
+                    this.anchor.appendChild(inputelement);
+                }
+            }
+        } 
+        get props(){
+            return this._props; 
+        } 
+    }
+    return {
+        FormComponent
+    }
+}
