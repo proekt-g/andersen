cb4caebe4dd41018d3711ae8e29ef4632a27fec5
Proekt_G <prog.dev.rob@gmail.com>
2019-12-02 20:35:29 +0200

    Сделать SearchListComponent класс. Этот класс должен иметь возможность исходя из принятых свойств построить элемент поиска и список. Этот список должен фильтроваться исходя из значений в инпут поле. Вам требуется создать модульИз которого будет возвращен класс компонента, имеющий функциональность для компонента поиска

---
+++ searchListComponent.js
@@ -0,0 +1,49 @@
+function module(){
+    class SearchListComponent {
+        constructor(element){
+            if(element !== undefined){
+                this.anchor = element;
+                this.anchor.appendChild(document.createElement("input"));
+                this.anchor.querySelector("input").addEventListener("change", (event) =>{
+                    if(event.target.value == '') this.props = this.firstList;
+                    else{
+                        let check = false;
+                        for(let value of this.firstList.listData){
+                            value.split(' ').forEach(nameProps => {
+                                if(nameProps == event.target.value) { 
+                                    this.props = { listData: [ value ] };
+                                    check = true; 
+                                }
+                            });
+                        } 
+                        if(!check) this.props = { listData: [ ] };
+                    }
+                });
+            }
+        }
+        onInit(){ }
+        onPropsChanges(){ }
+        set props(value){
+            if(this._props === undefined) this.firstList = value; 
+            this._props = value;
+            if(this.anchor !== undefined && this._props.listData !== undefined) this.anchor.querySelector("input").innerHTML = this.render();
+        }
+        get props(){
+            return this._props;
+        }
+        render(){
+            return `
+            <ul>
+                ${
+                    this._props.listData.map(item => `
+                        <li>${item}</li>
+                    `).join('')
+                }
+            </ul>
+            `;
+        }
+    }
+    return {
+        SearchListComponent
+    }
+}
