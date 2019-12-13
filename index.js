6a1a17eb0f4ba5b232e8e346de9628567726fe7a
Proekt_G <prog.dev.rob@gmail.com>
2019-12-13 16:10:48 +0200

    comint

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-const catsArray = [];
-
-function createCat(age, name) {
-    const cat = {
-        catsAge: age,
-        catsName: name,
-        meow: function() { return 'My name is ' + this.catsName + '!'; },
-        myAge: function() {
-            if (this.catsAge < 10) { return 'My age is ' + this.catsAge; }
-            return 'I am too old...';
-        },
-        myOrder() { return `I am cat №${catsArray.indexOf(this.catsAge) + 1} in family`; }
-    };
-    catsArray.push(cat.catsAge);
-    catsArray.sort(function (value1, value2) { return value2 - value1; });
-    return cat;    
+function module(){
+    class Observer {
+        constructor(){
+            this.observer = [];
+        }
+        subscribe(func){
+            this.observer.push(func);
+            this.unsubscribeObserver = func;
+            return this;
+        }
+        sbscribtionLength(){
+            return this.observer.length;
+        }
+        next(value){
+            this.observer.forEach((element, i) => {
+                if(this.changeObserver !== undefined && this.changeObserver.count == i){
+                    switch(this.changeObserver.type){
+                        case `map`: element(this.changeObserver.func(value)); break;
+                        case `filter`: if(this.changeObserver.func(value)) element(value); break;
+                    }
+                }else element(value);
+            });
+        }
+        map(func){
+            this.changeObserver = { func, count: this.observer.length, type: `map` };
+            return this;
+        }
+        filter(func){
+            this.changeObserver = { func, count: this.observer.length, type: `filter` };
+            return this;
+        }
+        unsubscribe(){
+            this.observer.forEach((element, i) => {
+                if(element === this.unsubscribeObserver) this.observer.splice(i, 1);
+            });
+        }
+    }
+    return {
+        Observer
+    }
 }
+
+let res1;
+let res2;
+const mod = module()
+const obs = new mod.Observer();
+const subscription1 = obs.subscribe(value => res1 = value);
+const subscription2 = obs.subscribe(value => res2 = value);
+obs.next(10);
+
+console.log(res1); //.toBe(10);
+console.log(res2); //.toBe(10);
+
+subscription2.unsubscribe()
+
+obs.next(12);
+console.log(res1); //.toBe(12);
+console.log(res2); //.toBe(10);
